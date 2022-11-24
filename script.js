f76b0529483b06c0b1c608c87c0bc080d7867efa
Pamaq <paulinakuta00@gmail.com>
2022-11-24 23:38:35 +0100

    Added feather icons and started creating pop up box

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const warningBtn = document.querySelector(".warning");
 const errorBtn = document.querySelector(".error");
 const defaultBtn = document.querySelector(".default");
 
-const warningFun = () => {
-	console.log("hello");
-};
-const errorFun = () => {console.log("welcome");};
-const defaultFun = () => {console.log(":))");};
-
-warningBtn.addEventListener("click", warningFun);
-errorBtn.addEventListener("click", errorFun);
-defaultBtn.addEventListener("click", defaultFun);
+warningBtn.addEventListener("click", );
+errorBtn.addEventListener("click",);
+defaultBtn.addEventListener("click",);
