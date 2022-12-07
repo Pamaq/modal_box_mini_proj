f6d9835d091da645250ea92c1bc4ef171d25cdcf
Pamaq <paulinakuta00@gmail.com>
2022-12-07 22:07:56 +0100

    Finished first pop-up modal

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const warningBtn = document.querySelector(".warning");
-const errorBtn = document.querySelector(".error");
-const defaultBtn = document.querySelector(".default");
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
-warningBtn.addEventListener("click", );
-errorBtn.addEventListener("click",);
-defaultBtn.addEventListener("click",);
+openModalButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const modal = document.querySelector(button.dataset.modalTarget);
+		openModal(modal);
+	});
+});
+
+overlay.addEventListener("click", () => {
+	const modals = document.querySelectorAll(".modal.active");
+	modals.forEach((modal) => closeModal(modal));
+});
+
+closeModalButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const modal = button.closest(".modal");
+		closeModal(modal);
+	});
+});
+function openModal(modal) {
+	if (modal == null) return;
+	modal.classList.add("active");
+	overlay.classList.add("active");
+}
+function closeModal(modal) {
+	if (modal == null) return;
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+}
