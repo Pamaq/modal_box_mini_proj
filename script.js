538e47c55917b5812357192205111eccb961c575
Pamaq <paulinakuta00@gmail.com>
2022-12-11 15:02:28 +0100

    Added carousel with pictures

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 const openModalButtons2 = document.querySelectorAll("[data-modal-target2]");
 const closeModalButtons2 = document.querySelectorAll("[data-close-button2]");
-const buttons = document.querySelector("[data-carousel-button]");
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
 openModalButtons.forEach((button) => {
 	button.addEventListener("click", () => {
 		const modal = document.querySelector(button.dataset.modalTarget);
@@ -66,13 +67,13 @@ buttons.forEach((button) => {
 	button.addEventListener("click", () => {
 		const offset = button.dataset.carouselButton === "next" ? 1 : -1;
 		const slides = button
-			.closest("[data-carousel")
-			.querySelector("[data-slides");
+			.closest("[data-carousel]")
+			.querySelector("[data-slides]");
 		const activeSlide = slides.querySelector("[data-active]");
 		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
 		if (newIndex < 0) newIndex = slides.children.length - 1;
 		if (newIndex >= slides.children.length) newIndex = 0;
-		slides.childen[newIndex].dataset.active = true;
+		slides.children[newIndex].dataset.active = true;
 		delete activeSlide.dataset.active;
 	});
 });
